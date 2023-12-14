0de8d17c5b10495f02c02be71fb6df9b93355690
Xonorchik <vagananarmen@gmail.com>
2023-12-15 01:43:48 +0400

    v1

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, HTTPException, Depends
-from sqlalchemy import create_engine, Column, Integer, String, Sequence
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session, declarative_base
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import Session
 from models import Patient, Treatment, Medic, TreatmentMedic
 from database import Base, engine, SessionLocal
+from pydantic import BaseModel
+from datetime import date
+from typing import List
+
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
@@ -16,3 +18,216 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+class PatientCreate(BaseModel):
+    full_name: str
+    date_of_birth: date
+    policy_number: int
+    social_status: str
+
+
+class PatientResponse(BaseModel):
+    id: int
+    full_name: str
+    date_of_birth: date
+    policy_number: int
+    social_status: str
+
+
+class PatientDelete(BaseModel):
+    message: str
+
+
+# Pydantic model for Result
+class TreatmentCreate(BaseModel):
+    diagnosis: str
+    current_state: str
+    date_start: date
+    date_end: date
+    patient_id: int
+    medic_id: int
+
+
+class TreatmentResponse(BaseModel):
+    id: int
+    diagnosis: str
+    current_state: str
+    date_start: date
+    date_end: date
+    patient_id: int
+    medic_id: int
+
+
+class TreatmentDelete(BaseModel):
+    message: str
+
+
+# Pydantic model for Athlete
+class MedicCreate(BaseModel):
+    full_name: str
+    speciality: str
+    exp_years: int
+
+
+class MedicResponse(BaseModel):
+    id: int
+    full_name: str
+    speciality: str
+    exp_years: int
+
+
+class MedicDelete(BaseModel):
+    message: str
+
+# Basic CRUD using FastAPI
+
+# Create
+
+
+@app.post("/patient/", response_model=PatientResponse)
+def create_patient(patient: PatientCreate, db: Session = Depends(get_db)):
+    db_patient = Patient(**patient.dict())
+    db.add(db_patient)
+    db.commit()
+    db.refresh(db_patient)
+    return db_patient
+
+
+@app.post("/treatment/", response_model=TreatmentResponse)
+def create_treatment(treatment: TreatmentCreate, db: Session = Depends(get_db)):
+    db_treatment = Treatment(**treatment.dict())
+    db.add(db_treatment)
+    db.commit()
+    db.refresh(db_treatment)
+    return db_treatment
+
+
+@app.post("/medic/", response_model=MedicResponse)
+def create_medic(medic: MedicCreate, db: Session = Depends(get_db)):
+    db_medic = Medic(**medic.dict())
+    db.add(db_medic)
+    db.commit()
+    db.refresh(db_medic)
+    return db_medic
+
+
+# Read
+@app.get("/patient/{patient_id}", response_model=PatientResponse)
+def get_patient(patient_id: int, db: Session = Depends(get_db)):
+    patient = db.query(Patient).filter(Patient.id == patient_id).first()
+    if patient is None:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    return patient
+
+
+@app.get("/medic/{medic_id}", response_model=MedicResponse)
+def get_medic(medic_id: int, db: Session = Depends(get_db)):
+    medic = db.query(Medic).filter(Medic.id == medic_id).first()
+    if medic is None:
+        raise HTTPException(status_code=404, detail='Medic not found')
+    return medic
+
+
+@app.get("/treatment/{treatment_id}", response_model=TreatmentResponse)
+def get_treatment(treatment_id: int, db: Session = Depends(get_db)):
+    treatment = db.query(Treatment).filter(Treatment.id == treatment_id).first()
+    if treatment is None:
+        raise HTTPException(status_code=404, detail='Treatment not found')
+    return treatment
+
+
+# Update
+@app.put("/patient/{patient_id}", response_model=PatientResponse)
+def update_patient(patient_id: int, updated: PatientCreate, db: Session = Depends(get_db)):
+    patient = db.query(Patient).filter(Patient.id == patient_id).first()
+    if patient is None:
+        raise HTTPException(status_code=404, detail='Patient not found')
+
+    for key, value in updated.dict().items():
+        setattr(patient, key, value)
+
+    db.commit()
+    db.refresh(patient)
+    return patient
+
+
+@app.put("/treatment/{treatment_id}", response_model=TreatmentResponse)
+def update_treatment(treatment_id: int, updated: TreatmentCreate, db: Session = Depends(get_db)):
+    treatment = db.query(Treatment).filter(Treatment.id == treatment_id).first()
+    if treatment is None:
+        raise HTTPException(status_code=404, detail='Treatment not found')
+
+    for key, value in updated.dict().items():
+        setattr(treatment, key, value)
+
+    db.commit()
+    db.refresh(treatment)
+    return treatment
+
+
+@app.put("/medic/{medic_id}", response_model=MedicResponse)
+def update_medic(medic_id: int, updated: MedicCreate, db: Session = Depends(get_db)):
+    medic = db.query(Medic).filter(medic_id == Medic.id).first()
+    if medic is None:
+        raise HTTPException(status_code=404, detail="Medic not found")
+
+    for key, value in updated.dict().items():
+        setattr(medic, key, value)
+
+    db.commit()
+    db.refresh(medic)
+    return medic
+
+
+# Delete
+@app.delete("/patient/{patient_id}", response_model=PatientDelete)
+def delete_patient(patient_id: int, db: Session = Depends(get_db)):
+    patient = db.query(Patient).filter(Patient.id == patient_id).first()
+    if patient is None:
+        raise HTTPException(status_code=404, detail="Patient not found")
+
+    db.delete(patient)
+    db.commit()
+    return {"message": "Patient deleted"}
+
+
+@app.delete("/treatment/{treatment_id}", response_model=TreatmentDelete)
+def delete_treatment(treatment_id: int, db: Session = Depends(get_db)):
+    treatment = db.query(Treatment).filter(Treatment.id == treatment_id).first()
+    if treatment is None:
+        raise HTTPException(status_code=404, detail="Treatment not found")
+
+    db.delete(treatment)
+    db.commit()
+    return {"message": "Treatment deleted"}
+
+
+@app.delete("/medic/{medic_id}", response_model=MedicDelete)
+def delete_medic(medic_id: int, db: Session = Depends(get_db)):
+    medic = db.query(Medic).filter(Medic.id == medic_id).first()
+    if medic is None:
+        raise HTTPException(status_code=404, detail="Medic not found")
+
+    db.delete(medic)
+    db.commit()
+    return {"message": "Medic deleted"}
+
+
+# Read with pagination
+@app.get("/patient/", response_model=List[PatientResponse])
+def get_patient(page: int = 0, per_page: int = 10, db: Session = Depends(get_db)):
+    patient = db.query(Patient).offset(page).limit(per_page).all()
+    return patient
+
+
+@app.get("/treatment/", response_model=List[TreatmentResponse])
+def get_treatment(page: int = 0, per_page: int = 10, db: Session = Depends(get_db)):
+    treatment = db.query(Treatment).offset(page).limit(per_page).all()
+    return treatment
+
+
+@app.get("/medic/", response_model=List[MedicResponse])
+def get_medic_sorted(page: int = 0, per_page: int = 10, sort_by: str = "id", db: Session = Depends(get_db)):
+    medic = db.query(Medic).order_by(getattr(Medic, sort_by)).offset(page).limit(per_page).all()
+    return medic
---
+++ populate.py
@@ -0,0 +1,67 @@
+import requests
+import random
+from faker import Faker
+from database import Base, engine
+from sqlalchemy.orm import sessionmaker
+
+
+BASE_URL = 'http://localhost:8000/'
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+fake = Faker()
+
+
+soc_stat = ['student', 'employer', 'temporarily unemployed', 'invalid', 'pensioner', 'child']
+current_stat = ['moderate', 'heavy condition', 'sent to stationary', 'died', 'recovered', 'discharged']
+dr_spec = ['Cardiologist', 'Neurologist', 'Orthopedic Surgeon', 'Pediatrician', 'Ophthalmologist',
+           'Dermatologist', 'Gastroenterologist', 'Endocrinologist', 'Oncologist', 'Urologist',
+           'Pulmonologist', 'Rheumatologist', 'Neurologist', 'Hematologist', 'Infectious Disease Specialist']
+
+
+def create_patient():
+    return {
+        "full_name": fake.name(),
+        "date_of_birth": fake.date_of_birth().strftime('%Y-%m-%d'),
+        "policy_number": fake.random_int(min=100000, max=999999),
+        "social_status": random.choice(soc_stat)
+    }
+
+
+def create_treatment(patient_id):
+    return {
+        "diagnosis": fake.word(),
+        "current_state": random.choice(current_stat),
+        "date_start": fake.date_between(start_date='-30d', end_date='today').strftime('%Y-%m-%d'),
+        "date_end": fake.date_between(start_date='today', end_date='+30d').strftime('%Y-%m-%d'),
+        "patient_id": patient_id,
+        "medic_id": random.randint(1, 100)
+    }
+
+
+def create_medic():
+    return {
+        "full_name": fake.name(),
+        "speciality": random.choice(dr_spec),
+        "exp_years": fake.random_int(min=1, max=20),
+    }
+
+
+def populate_database(num_patients):
+    for _ in range(num_patients):
+        patient_data = create_patient()
+        response = requests.post(BASE_URL + "patient/", json=patient_data)
+        patient_id = response.json().get("id")
+
+        treatment_data = create_treatment(patient_id)
+        requests.post(BASE_URL + "treatment/", json=treatment_data)
+
+
+if __name__ == "__main__":
+    num_medics_to_create = 100
+    for i in range(num_medics_to_create):
+        medic_data = create_medic()
+        requests.post(BASE_URL + "medic/", json=medic_data)
+    num_patients_to_create = 1000
+    populate_database(num_patients_to_create)
+    print('/Population completed/')
